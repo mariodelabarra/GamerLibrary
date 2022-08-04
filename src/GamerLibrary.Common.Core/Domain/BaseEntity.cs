e634dca1b4f19a2b5aa8eb06db42cce30562bc67
Mario <mario.delabarra@outlook.com>
2022-08-03 23:15:48 -0300

    Add Docker to Game project and create docker compose for SQL container

---
+++ src/GamerLibrary.Common.Core/Domain/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace GamerLibrary.Common.Core.Domain
+{
+    public class BaseEntity
+    {
+        /// <summary>
+        /// Uniquely identifier of the entity
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// UTC Datetime where the entity was created
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Last UTC Datetime where the entity was updated
+        /// </summary>
+        public DateTime? UpdatedDate { get; set; }
+
+        /// <summary>
+        /// UTC Datetime where the entity was deleted
+        /// </summary>
+        public DateTime? DeletedDate { get; set; }
+    }
+}
